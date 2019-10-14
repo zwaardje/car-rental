c9f5ea9d674163c943dde10549fb454adb47d738
Zwaardje <niels@nielsaulman.nl>
2019-10-14 08:48:52 +0200

    feat(overview): update overview
    Add load more fuctionality
    Add title and subtitle
    Add location component
    Add styling

--- src/components/organisms/Overview/Overview.tsx
+++ src/components/organisms/Overview/Overview.tsx
@@ -1,21 +1,47 @@
 import * as React from 'react';
 
+import { formatDate, DATE_FORMAT } from '../../../helpers/date';
+
+import { Button } from '../../atoms';
 import { Overview as OverviewInterface } from './OverviewInterface';
 
+import './Overview.scss';
+
 export interface Props extends OverviewInterface {}
-export interface State {}
+export interface State {
+    visible: number;
+}
 
 export default class Overview extends React.Component<Props, State> {
     static displayName = 'Overview';
 
+    state = {
+        visible: 1,
+    };
+
+    loadMore = () => this.setState({
+        visible: this.state.visible + 1,
+    })
+
     render() {
-        const { title, children } = this.props;
+        const { title, subtitle, children, startDate, endDate } = this.props;
+        const { visible } = this.state;
 
         return (
-            <div>
-                { title }
-                { children }
-            </div>
+            <section className="overview">
+                <h1 className="overview__heading">{ title }</h1>
+                { subtitle ? <p className="overview__subtitle">{subtitle} For the periode of {formatDate(new Date(startDate), DATE_FORMAT)} untill {formatDate(new Date(endDate), DATE_FORMAT)}</p> : null}
+                <div className="overview__list-item">
+                    { React.Children.map(children, (child, index) => {
+                        return index < visible ? child : null;
+                    })}
+                </div>
+                { React.Children.toArray(children).length > visible ?
+                    <Button className="overview__button" onClick={this.loadMore}>
+                        Load more options
+                    </Button>
+                : null }
+            </section>
         );
     }
 }
--- src/components/organisms/Overview/OverviewInterface.ts
+++ src/components/organisms/Overview/OverviewInterface.ts
@@ -1,7 +1,10 @@
 import { ReactNode } from 'react';
+import { string } from 'prop-types';
 
 export interface Overview {
     title: string;
     subtitle?: string;
     children: ReactNode;
+    startDate: string;
+    endDate: string;
 }
